55e59b0e3535fbd9643b807632e1d042f76d1a3a
Tom Bebbington <tophattedcoder@gmail.com>
2015-10-08 23:29:40 +0100

    Improve documentationer script

--- doc.sh
+++ doc.sh
@@ -1,11 +1,6 @@
-#! /bin/bash
+#! /bin/sh
 haxe doc.hxml
-mv -f doc /tmp/doc
-git checkout gh-pages
-rm -rf ./*
-mv -f /tmp/doc/* .
-rm -rf /tmp/doc
+haxelib run dox -i doc/doc.xml -o doc --title "Awe, the easy and fast ECS"
 git add -A .
-git commit -q -am "Update documentation"
-git push -fq origin gh-pages
-git checkout -f master
+git commit -am "Update documentation"
+git subtree push --prefix doc origin gh-pages
